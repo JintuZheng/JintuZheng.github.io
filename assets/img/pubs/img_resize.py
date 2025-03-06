b95c47a6c8788f1997d175c581b59002989431dd
Jintu <jintuzheng@outlook.com>
2025-03-07 00:23:28 +0800

    compression

--- assets/img/pubs/img_resize.py
+++ assets/img/pubs/img_resize.py
@@ -1,6 +1,6 @@
 from skimage import io
 import cv2
 
-im = io.imread('ucas_logo.jpg')
+im = io.imread('dipgo_proj.jpg')
 im = cv2.resize(im, (int(im.shape[1]//2), int(im.shape[0]//2)))
-io.imsave('ucas_logo.jpg', im)
+io.imsave('dipgo_proj.jpg', im)
